dbae8b8f0ac23acaa6531813fb2455cf82c91bd2
unknown <ggamals7744@yahoo.com>
2026-01-24 21:25:20 +0200

    table book project 2026

--- buttons.php
+++ buttons.php
@@ -1,3 +1,23 @@
 <button form='register' type='submit' class="btn btn-primary" onclick="validForm('#register')"><?php echo $view->getButtonName()?></button>
 <button type="button" onclick="openForm('#createModel')" class="btn btn-success"><?php echo $view->getChangeLanguageButton()?></button>
+<?php 
+    $title = $view->getModalTitleProject();
+    $button = $view->getModalButtonProject();
+    $action = 'SetupProject.php';
+    $idModel = "setupprojectmodal";
+    $idForm = "setupprojectform";
+    include('start_model.php');
+    echo '<input type="hidden" value="'.$view->getId().'"name="superId">
+    <input type="hidden" name="setup_project" value="'.$view->getUrlName2().'">'?>
+    <div class="form-group">
+        <label for="email"><?php echo $view->getNameLabel()?></label>
+        <input type="text" class="form-control" id="email" name="name"
+            placeholder="<?php echo $view->getNameHint()?>"
+            title="<?php echo $view->getNameHint()?>"
+            oninvalid="handleInput(this ,'<?php echo$view->getNameTableIsReq()?>', '<?php echo$view->getNameTableIsInv()?>')"
+            oninput="handleInput(this ,'<?php echo$view->getNameTableIsReq()?>', '<?php echo$view->getNameTableIsInv()?>')"
+            minlength="3"
+            required>
+    </div>
+    <?php include('end_model.php');?>
 <button onclick="openForm('#setupprojectmodal')" type="button" class="btn btn-danger" ><?php echo $view->getButtonSetupProject()?></button>
--- login_view.php
+++ login_view.php
@@ -32,26 +32,6 @@
         </div>
         <?php include('end_model.php');?>
         <button onclick="openForm('#forgetpasswordmodal')" type="button" class="btn btn-primary" ><?php echo $view->getButtonForgetPassword()?></button>
-        <?php 
-        $title = $view->getModalTitleProject();
-        $button = $view->getModalButtonProject();
-        $action = 'SetupProject.php';
-        $idModel = "setupprojectmodal";
-        $idForm = "setupprojectform";
-        include('start_model.php');
-        echo '<input type="hidden" value="'.$view->getId().'"name="superId">
-        <input type="hidden" name="setup_project" value="'.$view->getUrlName2().'">'?>
-        <div class="form-group">
-            <label for="email"><?php echo $view->getNameLabel()?></label>
-            <input type="text" class="form-control" id="email" name="name"
-                placeholder="<?php echo $view->getNameHint()?>"
-                title="<?php echo $view->getNameHint()?>"
-                oninvalid="handleInput(this ,'<?php echo$view->getNameTableIsReq()?>', '<?php echo$view->getNameTableIsInv()?>')"
-                oninput="handleInput(this ,'<?php echo$view->getNameTableIsReq()?>', '<?php echo$view->getNameTableIsInv()?>')"
-                minlength="3"
-                required>
-        </div>
-        <?php include('end_model.php');?>
     </div>
 </div>
 </body>
